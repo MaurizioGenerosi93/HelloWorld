136f46e32b1a96fd45a6b934cbb21453f0645341
MaurizioGenerosi93 <mgenerosi@studenti.apuliadigitalmaker.it>
2021-09-16 12:39:02 +0200

    Esempio1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,21 @@
 public class HelloWorld {
      public static void main (String[] args) {
 
-         System.out.println("Hello World!!!");
+         System.out.println("Ritengo illegale spiegare così!!!");
      }
 }
+
+/*public class HelloWorld {
+
+    public static int main (String [] args) {
+
+        int a = 5;
+        int b = 5;
+        int somma = 0;
+
+        return a+b;
+    }
+}*/
+
+
+
